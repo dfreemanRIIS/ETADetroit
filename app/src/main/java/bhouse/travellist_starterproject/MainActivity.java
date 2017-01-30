d7e6afa5a323a57a64f0efb38c9633d3b8c2d733
dfreemanRIIS <dfreeman@riis.com>
2017-01-30 14:33:11 -0500

    Now displays pictures

--- app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
+++ app/src/main/java/bhouse/travellist_starterproject/MainActivity.java
@@ -2,22 +2,30 @@ package bhouse.travellist_starterproject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-
 
 public class MainActivity extends Activity {
 
   private Menu menu;
   private boolean isListView;
+  private RecyclerView mRecyclerView;
+    private StaggeredGridLayoutManager mStaggeredLayoutManager;
+    private TravelListAdapter mAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-
     isListView = true;
+      mRecyclerView = (RecyclerView) findViewById(R.id.list);
+      mStaggeredLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL);
+      mRecyclerView.setLayoutManager(mStaggeredLayoutManager);
+      mAdapter = new TravelListAdapter(this);
+      mRecyclerView.setAdapter(mAdapter);
   }
 
   private void setUpActionBar() {
