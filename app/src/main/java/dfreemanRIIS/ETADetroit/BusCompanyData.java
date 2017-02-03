4927b2ecaa734984f75481f00dd03b2598c2e00b
dfreemanRIIS <dfreeman@riis.com>
2017-02-03 14:58:35 -0500

    BusCompanyData is now a POJO

--- app/src/main/java/dfreemanRIIS/ETADetroit/BusCompanyData.java
+++ app/src/main/java/dfreemanRIIS/ETADetroit/BusCompanyData.java
@@ -1,19 +1,28 @@
 package dfreemanRIIS.ETADetroit;
 
+import android.content.Context;
+import android.database.Cursor;
 import java.util.ArrayList;
 
 class BusCompanyData {
 
-  private final String[] placeNameArray = {"DDOT", "RefleX", "SmartBus"};
+    private final String[] placeNameArray = {"DDOT", "RefleX", "SmartBus"};
+    private Context context;
 
-  public ArrayList<BusCompany> placeList() {
-    ArrayList<BusCompany> list = new ArrayList<>();
-    for (String aPlaceNameArray : placeNameArray) {
-      BusCompany busCompany = new BusCompany();
-      busCompany.name = aPlaceNameArray;
-      busCompany.imageName = aPlaceNameArray.replaceAll("\\s+", "").toLowerCase();
-      list.add(busCompany);
+    public BusCompanyData(Context context) {
+        this.context = context;
+        Controller controller = new Controller();
+        Cursor companyCursor = controller.getCompanies(context);
     }
-    return (list);
-  }
+
+    public ArrayList<BusCompany> placeList() {
+        ArrayList<BusCompany> list = new ArrayList<>();
+        for (String aPlaceNameArray : placeNameArray) {
+            BusCompany busCompany = new BusCompany();
+            busCompany.name = aPlaceNameArray;
+            busCompany.imageName = aPlaceNameArray.replaceAll("\\s+", "").toLowerCase();
+            list.add(busCompany);
+        }
+        return (list);
+    }
 }
