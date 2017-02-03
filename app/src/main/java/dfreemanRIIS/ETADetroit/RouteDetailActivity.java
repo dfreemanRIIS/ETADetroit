e888b60403f1dd1527c2650dd17be37c22728a6e
dfreemanRIIS <dfreeman@riis.com>
2017-02-03 10:24:51 -0500

    Remove redundent list views

--- app/src/main/java/dfreemanRIIS/ETADetroit/RouteDetailActivity.java
+++ app/src/main/java/dfreemanRIIS/ETADetroit/RouteDetailActivity.java
@@ -23,24 +23,14 @@ public class RouteDetailActivity extends Activity {
         Cursor details = controller.getRouteDetails(thisRoute, this);
 
         if(details.moveToFirst()) {
-            TextView textView1 = (TextView)findViewById(R.id.listDetails1);
-            TextView textView2 = (TextView)findViewById(R.id.listDetails2);
-            TextView textView3 = (TextView)findViewById(R.id.listDetails3);
-            TextView textView4 = (TextView)findViewById(R.id.listDetails4);
-            TextView textView5 = (TextView)findViewById(R.id.listDetails5);
-            TextView textView6 = (TextView)findViewById(R.id.listDetails6);
-            String placeHolder1 = "Company: " + details.getString(1);
-            String placeHolder2 = "Route name: " + details.getString(2);
-            String placeHolder3 = "Route number: " + details.getString(3);
-            String placeHolder4 = "Direction 1: " + details.getString(4);
-            String placeHolder5 = "Direction 2: " + details.getString(5);
-            String placeHolder6 = "Days active: " + details.getString(6);
+            TextView textView1 = (TextView)findViewById(R.id.listDetails);
+            String placeHolder1 = "Company: " + details.getString(1)
+                    + "\nRoute name: " + details.getString(2)
+                    + "\nRoute number: " + details.getString(3)
+                    + "\nDirection 1: " + details.getString(4)
+                    + "\nDirection 2: " + details.getString(5)
+                    + "\nDays active: " + details.getString(6);
             textView1.setText(placeHolder1);
-            textView2.setText(placeHolder2);
-            textView3.setText(placeHolder3);
-            textView4.setText(placeHolder4);
-            textView5.setText(placeHolder5);
-            textView6.setText(placeHolder6);
         }
 
 		Cursor stops = controller.getRouteStops(details.getString(3), this);
